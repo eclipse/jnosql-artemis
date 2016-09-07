cc2bbf076055d3641a04545386a2bc9177b1298d
Otavio <otaviojava@java.net>
2016-09-07 17:21:37 -0300

    adds default field representation

--- artemis-core/src/main/java/org/jnosql/artemis/reflection/DefaultFieldRepresentation.java
+++ artemis-core/src/main/java/org/jnosql/artemis/reflection/DefaultFieldRepresentation.java
@@ -3,12 +3,63 @@ package org.jnosql.artemis.reflection;
 
 import java.io.Serializable;
 import java.lang.reflect.Field;
+import java.util.Objects;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
-public class DefaultFieldRepresentation implements Serializable {
+public class DefaultFieldRepresentation implements FieldRepresentation {
 
-    private FieldType type;
+    private final FieldType type;
 
-    private Field field;
+    private final Field field;
 
-    private String name;
+    private final String name;
+
+    DefaultFieldRepresentation(FieldType type, Field field, String name) {
+        this.type = type;
+        this.field = field;
+        this.name = name;
+    }
+
+    @Override
+    public FieldType getType() {
+        return type;
+    }
+
+    @Override
+    public Field getField() {
+        return field;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        DefaultFieldRepresentation that = (DefaultFieldRepresentation) o;
+        return type == that.type &&
+                Objects.equals(field, that.field) &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, field, name);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("type", type)
+                .append("field", field)
+                .append("name", name)
+                .toString();
+    }
 }
