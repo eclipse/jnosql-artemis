27c585a8731825936b966e575c848bac7199d658
Otavio <otaviojava@java.net>
2016-09-07 16:27:35 -0300

    Adds initial annotation

---
+++ artemis-core/src/main/java/org/jnosql/artemis/Field.java
@@ -0,0 +1,14 @@
+package org.jnosql.artemis;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Field {
+
+    String value() default "";
+}
