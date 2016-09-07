a3e776fbf8f04656d1a30aff864035486f363f6a
Otavio <otaviojava@java.net>
2016-09-07 17:33:41 -0300

    Adds Embedded annotation

---
+++ artemis-core/src/main/java/org/jnosql/artemis/Embedded.java
@@ -0,0 +1,15 @@
+package org.jnosql.artemis;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Embedded {
+
+    String value() default "";
+
+}
