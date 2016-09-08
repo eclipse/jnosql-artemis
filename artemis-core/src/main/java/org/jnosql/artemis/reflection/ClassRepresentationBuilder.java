af5ed3ddb28dbdec75d5d0d07eb0663c32ded602
Otavio <otaviojava@java.net>
2016-09-08 16:47:15 -0300

    Adds class representation builder

---
+++ artemis-core/src/main/java/org/jnosql/artemis/reflection/ClassRepresentationBuilder.java
@@ -0,0 +1,37 @@
+package org.jnosql.artemis.reflection;
+
+import java.util.List;
+
+class ClassRepresentationBuilder {
+    private String name;
+
+    private List<String> fieldsName;
+
+    private Class<?> classInstance;
+
+    private List<FieldRepresentation> fields;
+
+    public ClassRepresentationBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ClassRepresentationBuilder withFieldsName(List<String> fieldsName) {
+        this.fieldsName = fieldsName;
+        return this;
+    }
+
+    public ClassRepresentationBuilder withClassInstance(Class<?> classInstance) {
+        this.classInstance = classInstance;
+        return this;
+    }
+
+    public ClassRepresentationBuilder withFields(List<FieldRepresentation> fields) {
+        this.fields = fields;
+        return this;
+    }
+
+    public ClassRepresentation build() {
+        return new ClassRepresentation(name, fieldsName, classInstance, fields);
+    }
+}
