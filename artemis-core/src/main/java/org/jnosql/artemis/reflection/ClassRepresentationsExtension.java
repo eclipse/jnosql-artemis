25f400062ec3942b7c7f73c3b760f5b06235e983
Otavio <otaviojava@java.net>
2016-09-09 22:46:56 -0300

    Adds documentation in reflection package

--- artemis-core/src/main/java/org/jnosql/artemis/reflection/ClassRepresentationsExtension.java
+++ artemis-core/src/main/java/org/jnosql/artemis/reflection/ClassRepresentationsExtension.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jnosql.artemis.reflection;
 
 
@@ -9,6 +27,9 @@ import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.ProcessAnnotatedType;
 import org.jnosql.artemis.Entity;
 
+/**
+ * This class is a CDI extension to load all class that has {@link Entity} annotation
+ */
 @ApplicationScoped
 public class ClassRepresentationsExtension implements Extension {
 
@@ -24,15 +45,20 @@ public class ClassRepresentationsExtension implements Extension {
     }
 
 
-
+    /**
+     * Event observer
+     *
+     * @param target the target
+     * @param <T>    the type
+     */
     public <T> void initializePropertyLoading(final @Observes ProcessAnnotatedType<T> target) {
 
         AnnotatedType<T> at = target.getAnnotatedType();
-        if(!at.isAnnotationPresent(Entity.class)) {
+        if (!at.isAnnotationPresent(Entity.class)) {
             return;
         }
         Class<T> javaClass = target.getAnnotatedType().getJavaClass();
-        LOGGER.info("scanning type: "  + javaClass.getName());
+        LOGGER.info("scanning type: " + javaClass.getName());
         CLASS_REPRESENTATIONS.load(javaClass);
     }
 
