2f8a7515ece9d325f23a5b1ae645fbd552587a4e
Otavio Santana <otaviojava@java.net>
2017-02-19 20:10:14 -0300

    improves documetation

---
+++ artemis-validation/src/main/java/org/jnosql/artemis/validation/package-info.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2017 Otavio Santana and others
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * The artemis-validation is a subproject of Artemis that has the goals to validate the data using bean validation.
+ * It uses the Bean validation API to do this validation before the event is persist:
+ * So, it listens:
+ * <p>KeyValueEntityPrePersit</p>
+ * <p>DocumentEntityPrePersist</p>
+ * <p>ColumnEntityPrePersist</p>
+ */
+package org.jnosql.artemis.validation;
