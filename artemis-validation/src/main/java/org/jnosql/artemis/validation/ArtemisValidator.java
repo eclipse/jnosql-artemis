d245c63cb6f2064063168001570996cfde577d85
Otavio Santana <otaviojava@java.net>
2017-02-19 20:58:22 -0300

    creates a artemis validatio interface

---
+++ artemis-validation/src/main/java/org/jnosql/artemis/validation/ArtemisValidator.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017 Otavio Santana and others
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jnosql.artemis.validation;
+
+
+import javax.validation.Validator;
+
+/**
+ * Validates bean instances. Implementations of this interface must be thread-safe.
+ */
+public interface ArtemisValidator {
+
+
+    /**
+     * Validate a bean using bean validation
+     *
+     * @param bean the be to be validated
+     * @param <T>  the type
+     * @throws NullPointerException       when bean is null
+     * @throws ArtemisValidationException when {@link Validator#validate(Object, Class[])}
+     *                                    returns a non empty collection
+     */
+    <T> void validate(T bean) throws NullPointerException, ArtemisValidationException;
+}
--- artemis-validation/src/main/java/org/jnosql/artemis/validation/DefaultArtemisValidator.java
+++ artemis-validation/src/main/java/org/jnosql/artemis/validation/DefaultArtemisValidator.java
@@ -26,13 +26,11 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
-import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toSet;
 
-class BeanValidationProducer {
+class DefaultArtemisValidator implements ArtemisValidator{
 
     @Inject
     private Instance<ValidatorFactory> validatorFactories;
