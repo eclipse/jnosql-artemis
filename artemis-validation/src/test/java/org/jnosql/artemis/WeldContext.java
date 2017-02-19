338b5aad1fcdd6de89e3e504d208b38a6494c1e5
Otavio Santana <otaviojava@java.net>
2017-02-19 20:00:55 -0300

    adds validation

---
+++ artemis-validation/src/test/java/org/jnosql/artemis/WeldContext.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017 Otavio Santana and others
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jnosql.artemis;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
+public class WeldContext {
+
+    public static final WeldContext INSTANCE = new WeldContext();
+
+    private final Weld weld;
+    private final WeldContainer container;
+
+    private WeldContext() {
+        this.weld = new Weld();
+        this.container = weld.initialize();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                weld.shutdown();
+            }
+        });
+    }
+
+    public <T> T getBean(Class<T> type) {
+        return container.instance().select(type).get();
+    }
+}
