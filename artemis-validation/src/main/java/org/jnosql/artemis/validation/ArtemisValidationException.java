f44e0d04d0f872740766a425141210bff0387c13
Otavio Santana <otaviojava@java.net>
2017-02-19 20:50:31 -0300

    adds a new exception

---
+++ artemis-validation/src/main/java/org/jnosql/artemis/validation/ArtemisValidationException.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017 Otavio Santana and others
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jnosql.artemis.validation;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ValidationException;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Base exception of all Bean Validation "unexpected" problems at Artemis.
+ */
+public class ArtemisValidationException extends ValidationException {
+
+    private final Set<ConstraintViolation<?>> violations;
+
+
+    /**
+     * The creates a new exception
+     *
+     * @param violations the validation
+     * @throws NullPointerException when violations is null
+     */
+    public ArtemisValidationException(Set<ConstraintViolation<?>> violations) throws NullPointerException {
+        super(requireNonNull(violations, "validation is required")
+                .stream()
+                .map(c -> c.getPropertyPath() + ": " + c.getMessage())
+                .collect(Collectors.joining("\n")));
+        this.violations = violations;
+    }
+
+
+
+    /**
+     * Getter of violations
+     *
+     * @return the violations
+     */
+    public Set<ConstraintViolation<?>> getViolations() {
+        return violations;
+    }
+
+}
---
+++ artemis-validation/src/main/java/org/jnosql/artemis/validation/BeanValidationProducer.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 Otavio Santana and others
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jnosql.artemis.validation;
+
+
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toSet;
+
+class BeanValidationProducer {
+
+    @Inject
+    private Instance<ValidatorFactory> validatorFactories;
+
+    @Inject
+    private Instance<Validator> validators;
+
+    public void validate(Object bean) {
+        Validator validator = getValidator();
+        Set<ConstraintViolation<Object>> violations = validator.validate(bean);
+
+        if (!violations.isEmpty()) {
+            throw new ArtemisValidationException(violations.stream().collect(toSet()));
+        }
+
+    }
+
+
+    private Validator getValidator() {
+        if (!validators.isUnsatisfied()) {
+            return validators.get();
+        } else if (!validatorFactories.isUnsatisfied()) {
+            ValidatorFactory validatorFactory = validatorFactories.get();
+            return validatorFactory.getValidator();
+        } else {
+            ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+            return factory.getValidator();
+        }
+    }
+}
