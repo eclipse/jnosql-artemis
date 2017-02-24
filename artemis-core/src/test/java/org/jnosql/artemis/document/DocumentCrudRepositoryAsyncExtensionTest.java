ef860de9efb3894b6cf4a93963c288227c3a26e6
Otavio Santana <otaviojava@java.net>
2017-02-24 17:01:51 -0300

    creates testes

---
+++ artemis-core/src/test/java/org/jnosql/artemis/document/DocumentCrudRepositoryAsyncExtensionTest.java
@@ -0,0 +1,10 @@
+package org.jnosql.artemis.document;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by otaviojava on 2/24/17.
+ */
+public class DocumentCrudRepositoryAsyncExtensionTest {
+
+}
