bc1e43ed356ea8cb3ef13269e1fdba782f102f22
Otavio Santana <otaviojava@java.net>
2017-03-05 10:55:49 -0300

    Adds convert

---
+++ artemis-core/src/main/java/org/jnosql/artemis/AttributeConverter.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 Otavio Santana and others
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jnosql.artemis;
+
+
+/**
+ * A class that implements this interface can be used to convert entity attribute state into database column
+ * representation and back again. Note that the X and Y types may be the same Java type.
+ * <X> the type of the entity attribute
+ * <Y> the type of the database column
+ */
+public interface AttributeConverter<X, Y> {
+
+    /**
+     * Converts the value stored in the entity attribute into the data representation to be stored in the database.
+     * @param attribute attribute the entity attribute value to be converted
+     * @return the converted data to be stored in the database column
+     */
+    Y toDatabaseColumn (X attribute);
+
+    /**
+     * Converts the data stored in the database column into the value to be stored in the entity attribute.
+     * Note that it is the responsibility of the converter writer to specify the correct dbData type for the
+     * corresponding column for use by the JDBC driver: i.e., persistence providers are not expected to do
+     * such type conversion.
+     * @param dbData  the data from the database column to be converted
+     * @return the converted value to be stored in the entity attribute
+     */
+    X toEntityAttribute (Y dbData);
+}
