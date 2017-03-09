8dad1da2329760d087c66ae75b2cb3ba4dc1f6b1
Otavio Santana <otaviojava@java.net>
2017-03-09 17:43:34 -0300

    fixes checkstyle

--- artemis-core/src/main/java/org/jnosql/artemis/column/query/ColumnCrudRepositoryProxy.java
+++ artemis-core/src/main/java/org/jnosql/artemis/column/query/ColumnCrudRepositoryProxy.java
@@ -30,7 +30,6 @@ import org.jnosql.diana.api.column.ColumnQuery;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
-import java.time.Duration;
 
 
 /**
