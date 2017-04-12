d6b28fce65cf855c05c946b79e26c3e2dada166d
Otavio Santana <otaviojava@java.net>
2017-04-12 19:03:34 -0300

    Fixes imports

--- artemis-core/src/main/java/org/jnosql/artemis/document/query/ReturnTypeConverterUtil.java
+++ artemis-core/src/main/java/org/jnosql/artemis/document/query/ReturnTypeConverterUtil.java
@@ -26,7 +26,6 @@ import java.util.Optional;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
