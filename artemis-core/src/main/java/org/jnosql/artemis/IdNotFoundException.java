5cbd4d9f5a2ba6fa36f5dc5b0dab958b6a57c53f
Otavio Santana <otaviojava@java.net>
2017-05-08 08:12:12 -0400

    moves the package

--- artemis-core/src/main/java/org/jnosql/artemis/IdNotFoundException.java
+++ artemis-core/src/main/java/org/jnosql/artemis/IdNotFoundException.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jnosql.artemis.key;
+package org.jnosql.artemis;
 
 import org.jnosql.artemis.ArtemisException;
 
