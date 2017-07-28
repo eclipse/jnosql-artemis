ddfacd9d5924d208cd5f493fb025d2a344dfdc4f
Otavio Santana <otaviopolianasantana@gmail.com>
2017-07-28 07:24:32 -0300

    Fixes checkstyle

--- artemis-core/src/main/java/org/jnosql/artemis/document/DefaultDocumentEntityConverter.java
+++ artemis-core/src/main/java/org/jnosql/artemis/document/DefaultDocumentEntityConverter.java
@@ -32,7 +32,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
