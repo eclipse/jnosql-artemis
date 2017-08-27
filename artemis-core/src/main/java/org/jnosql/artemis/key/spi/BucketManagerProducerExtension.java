7e4ddd856845125b8b1d52ea46e27257ee820bac
Otavio Santana <otaviopolianasantana@gmail.com>
2017-08-27 07:11:09 -0300

    Fixes bucker extension

--- artemis-core/src/main/java/org/jnosql/artemis/key/spi/BucketManagerProducerExtension.java
+++ artemis-core/src/main/java/org/jnosql/artemis/key/spi/BucketManagerProducerExtension.java
@@ -17,7 +17,7 @@ package org.jnosql.artemis.key.spi;
 
 import org.jnosql.artemis.Database;
 import org.jnosql.artemis.Databases;
-import org.jnosql.artemis.key.query.KeyValueRepository;
+import org.jnosql.artemis.Repository;
 import org.jnosql.artemis.key.query.KeyValueRepositoryBean;
 import org.jnosql.diana.api.key.BucketManager;
 
@@ -38,7 +38,7 @@ import java.util.stream.Stream;
 import static org.jnosql.artemis.DatabaseType.KEY_VALUE;
 
 /**
- * Extension to start up {@link org.jnosql.artemis.key.KeyValueTemplate} and {@link KeyValueRepository}
+ * Extension to start up {@link org.jnosql.artemis.key.KeyValueTemplate} and {@link org.jnosql.artemis.Repository}
  * from the {@link javax.enterprise.inject.Default} and {@link Database} qualifier
  */
 public class BucketManagerProducerExtension implements Extension {
@@ -53,14 +53,14 @@ public class BucketManagerProducerExtension implements Extension {
         Databases.addDatabase(pp, KEY_VALUE, databases);
     }
 
-    <T extends KeyValueRepository> void onProcessAnnotatedType(@Observes final ProcessAnnotatedType<T> repo) {
+    <T extends Repository> void onProcessAnnotatedType(@Observes final ProcessAnnotatedType<T> repo) {
         Class<T> javaClass = repo.getAnnotatedType().getJavaClass();
 
-        if (KeyValueRepository.class.equals(javaClass)) {
+        if (Repository.class.equals(javaClass)) {
             return;
         }
 
-        if (Stream.of(javaClass.getInterfaces()).anyMatch(KeyValueRepository.class::equals)
+        if (Stream.of(javaClass.getInterfaces()).anyMatch(Repository.class::equals)
                 && Modifier.isInterface(javaClass.getModifiers())) {
             LOGGER.info("Adding a new KeyValueRepository as discovered on Column: " + javaClass);
             crudTypes.add(repo.getAnnotatedType().getJavaClass());
