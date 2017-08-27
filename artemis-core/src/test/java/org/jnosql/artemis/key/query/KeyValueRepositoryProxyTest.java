fd3073ccff129ffb95a24a0a2e57bd4f32090d2f
Otavio Santana <otaviopolianasantana@gmail.com>
2017-08-27 07:11:02 -0300

    Fixes tests

--- artemis-core/src/test/java/org/jnosql/artemis/key/query/KeyValueRepositoryProxyTest.java
+++ artemis-core/src/test/java/org/jnosql/artemis/key/query/KeyValueRepositoryProxyTest.java
@@ -15,6 +15,7 @@
 package org.jnosql.artemis.key.query;
 
 import org.hamcrest.Matchers;
+import org.jnosql.artemis.Repository;
 import org.jnosql.artemis.key.KeyValueTemplate;
 import org.jnosql.artemis.model.User;
 import org.junit.Before;
@@ -53,91 +54,67 @@ public class KeyValueRepositoryProxyTest {
     }
 
     @Test
-    public void shouldPut() {
+    public void shouldSave() {
         ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
 
         User user = new User("ada", "Ada", 10);
-        userRepository.put(user);
+        userRepository.save(user);
         Mockito.verify(repository).put(captor.capture());
         User value = captor.getValue();
         assertEquals(user, value);
     }
 
-    @Test
-    public void shouldPutWithDdl() {
-        ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
-        ArgumentCaptor<Duration> durationCaptor = ArgumentCaptor.forClass(Duration.class);
-
-        User user = new User("ada", "Ada", 10);
-        userRepository.put(user, Duration.ofHours(2));
-        Mockito.verify(repository).put(captor.capture(), durationCaptor.capture());
-        User value = captor.getValue();
-        assertEquals(user, value);
-        assertEquals(Duration.ofHours(2), durationCaptor.getValue());
-    }
 
     @Test
-    public void shouldPutIterable() {
+    public void shouldSaveIterable() {
         ArgumentCaptor<Iterable> captor = ArgumentCaptor.forClass(Iterable.class);
 
         User user = new User("ada", "Ada", 10);
-        userRepository.put(Collections.singleton(user));
+        userRepository.save(Collections.singleton(user));
         Mockito.verify(repository).put(captor.capture());
         User value = (User) captor.getValue().iterator().next();
         assertEquals(user, value);
     }
 
-    @Test
-    public void shouldPutIterableWithDdl() {
-        ArgumentCaptor<Iterable> captor = ArgumentCaptor.forClass(Iterable.class);
-        ArgumentCaptor<Duration> durationCaptor = ArgumentCaptor.forClass(Duration.class);
-
-        User user = new User("ada", "Ada", 10);
-        userRepository.put(Collections.singleton(user), Duration.ofHours(2));
-        Mockito.verify(repository).put(captor.capture(), durationCaptor.capture());
-        User value = (User) captor.getValue().iterator().next();
-        assertEquals(user, value);
-        assertEquals(Duration.ofHours(2), durationCaptor.getValue());
-    }
 
 
     @Test
-    public void shouldRemove() {
-        userRepository.remove("key");
+    public void shouldDeleve() {
+        userRepository.deleteById("key");
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
         Mockito.verify(repository).remove(captor.capture());
         assertEquals("key", captor.getValue());
     }
 
     @Test
-    public void shouldRemoveIterable() {
-        userRepository.remove(Collections.singletonList("key"));
+    public void shouldDeleteIterable() {
+        userRepository.deleteById(Collections.singletonList("key"));
         ArgumentCaptor<Iterable> captor = ArgumentCaptor.forClass(Iterable.class);
         Mockito.verify(repository).remove(captor.capture());
         assertEquals("key", captor.getValue().iterator().next());
     }
 
     @Test
-    public void shouldGet() {
+    public void shouldFindById() {
         User user = new User("ada", "Ada", 10);
         Mockito.when(repository.get("key", User.class)).thenReturn(
                 Optional.of(user));
 
-        assertEquals(user, userRepository.get("key").get());
+        assertEquals(user, userRepository.findById("key").get());
     }
 
     @Test
-    public void shouldGetIterable() {
+    public void shouldFindByIdterable() {
         User user = new User("ada", "Ada", 10);
         User user2 = new User("ada", "Ada", 10);
         List<String> keys = Arrays.asList("key", "key2");
         Mockito.when(repository.get(keys, User.class)).thenReturn(
                 Arrays.asList(user, user2));
 
-        assertThat(userRepository.get(keys), Matchers.containsInAnyOrder(user, user2));
+        assertThat(userRepository.findById(keys), Matchers.containsInAnyOrder(user, user2));
     }
 
-    interface UserRepository extends KeyValueRepository<User> {
+    interface UserRepository extends Repository<User, String> {
 
     }
 
