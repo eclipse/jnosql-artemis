5169b9dbf5922fd8ce98b5efa2e40a36a58db6a3
Otavio Santana <otaviopolianasantana@gmail.com>
2017-10-27 15:11:47 -0200

    Fixes tests classes

--- artemis-key-value/src/test/java/org/jnosql/artemis/MockProducer.java
+++ artemis-key-value/src/test/java/org/jnosql/artemis/MockProducer.java
@@ -15,120 +15,19 @@
 package org.jnosql.artemis;
 
 
-import org.jnosql.artemis.document.DocumentTemplate;
-import org.jnosql.artemis.document.DocumentTemplateAsync;
 import org.jnosql.artemis.model.Person;
 import org.jnosql.artemis.model.User;
 import org.jnosql.diana.api.Value;
-import org.jnosql.diana.api.column.ColumnEntity;
-import org.jnosql.diana.api.column.ColumnFamilyManager;
-import org.jnosql.diana.api.column.ColumnFamilyManagerAsync;
-import org.jnosql.diana.api.document.Document;
-import org.jnosql.diana.api.document.DocumentCollectionManager;
-import org.jnosql.diana.api.document.DocumentCollectionManagerAsync;
-import org.jnosql.diana.api.document.DocumentEntity;
-import org.jnosql.diana.api.document.DocumentQuery;
 import org.jnosql.diana.api.key.BucketManager;
 import org.mockito.Mockito;
 
 import javax.enterprise.inject.Produces;
-
 import java.util.Optional;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class MockProducer {
 
-    @Produces
-    public DocumentCollectionManager getDocumentCollectionManager() {
-        DocumentEntity entity = DocumentEntity.of("Person");
-        entity.add(Document.of("name", "Default"));
-        entity.add(Document.of("age", 10));
-        DocumentCollectionManager manager = mock(DocumentCollectionManager.class);
-        when(manager.insert(Mockito.any(DocumentEntity.class))).thenReturn(entity);
-        return manager;
-
-    }
-
-    @Produces
-    @Database(value = DatabaseType.DOCUMENT, provider = "documentRepositoryMock")
-    public DocumentCollectionManager getDocumentCollectionManagerMock() {
-        DocumentEntity entity = DocumentEntity.of("Person");
-        entity.add(Document.of("name", "documentRepositoryMock"));
-        entity.add(Document.of("age", 10));
-        DocumentCollectionManager manager = mock(DocumentCollectionManager.class);
-        when(manager.insert(Mockito.any(DocumentEntity.class))).thenReturn(entity);
-        return manager;
-
-    }
-
-    @Produces
-    public ColumnFamilyManager getColumnFamilyManager() {
-        ColumnEntity entity = ColumnEntity.of("Person");
-        entity.add(org.jnosql.diana.api.column.Column.of("name", "Default"));
-        entity.add(org.jnosql.diana.api.column.Column.of("age", 10));
-        ColumnFamilyManager manager = mock(ColumnFamilyManager.class);
-        when(manager.insert(Mockito.any(ColumnEntity.class))).thenReturn(entity);
-        return manager;
-
-    }
-
-    @Produces
-    @Database(value = DatabaseType.COLUMN, provider = "columnRepositoryMock")
-    public ColumnFamilyManager getColumnFamilyManagerMock() {
-        ColumnEntity entity = ColumnEntity.of("Person");
-        entity.add(org.jnosql.diana.api.column.Column.of("name", "columnRepositoryMock"));
-        entity.add(org.jnosql.diana.api.column.Column.of("age", 10));
-        ColumnFamilyManager manager = mock(ColumnFamilyManager.class);
-        when(manager.insert(Mockito.any(ColumnEntity.class))).thenReturn(entity);
-        return manager;
-
-    }
-
-
-    @Produces
-    @Database(value = DatabaseType.DOCUMENT, provider = "documentRepositoryMock")
-    public DocumentTemplate getDocumentRepository() {
-        DocumentTemplate documentTemplate = mock(DocumentTemplate.class);
-        when(documentTemplate.insert(Mockito.any(Person.class))).thenReturn(Person.builder()
-                .withName("documentRepositoryMock").build());
-
-        when(documentTemplate.singleResult(any(DocumentQuery.class))).thenReturn(Optional.empty());
-
-        return documentTemplate;
-    }
-
-
-    @Produces
-    public DocumentCollectionManagerAsync getDocumentCollectionManagerAsync() {
-        return Mockito.mock(DocumentCollectionManagerAsync.class);
-    }
-
-
-    @Produces
-    @Database(value = DatabaseType.DOCUMENT, provider = "documentRepositoryMock")
-    public DocumentCollectionManagerAsync getDocumentCollectionManagerAsyncMock() {
-        return Mockito.mock(DocumentCollectionManagerAsync.class);
-    }
-    @Produces
-    public ColumnFamilyManagerAsync getColumnFamilyManagerAsync() {
-        return Mockito.mock(ColumnFamilyManagerAsync.class);
-    }
-
-
-    @Produces
-    @Database(value = DatabaseType.COLUMN, provider = "columnRepositoryMock")
-    public ColumnFamilyManagerAsync getColumnFamilyManagerAsyncMock() {
-        return Mockito.mock(ColumnFamilyManagerAsync.class);
-    }
-
-    @Produces
-    @Database(value = DatabaseType.DOCUMENT, provider = "documentRepositoryMock")
-    public DocumentTemplateAsync getDocumentRepositoryAsync() {
-        return mock(DocumentTemplateAsync.class);
-    }
 
 
     @Produces
