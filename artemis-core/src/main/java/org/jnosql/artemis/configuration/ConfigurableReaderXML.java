42c869a1e418249271bfa77f2b21cf4477773df9
Otavio Santana <otaviopolianasantana@gmail.com>
2017-10-29 22:34:39 -0200

    fixes imports

--- artemis-core/src/main/java/org/jnosql/artemis/configuration/ConfigurableReaderXML.java
+++ artemis-core/src/main/java/org/jnosql/artemis/configuration/ConfigurableReaderXML.java
@@ -25,7 +25,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 import java.util.logging.Logger;
