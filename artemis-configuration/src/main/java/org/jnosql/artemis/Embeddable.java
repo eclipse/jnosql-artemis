4f47e184cf1824bfa88b2ce5695156704733acfc
Otavio Santana <otaviopolianasantana@gmail.com>
2017-11-02 19:35:54 -0200

    Adds artemis-configuration

---
+++ artemis-configuration/src/main/java/org/jnosql/artemis/Embeddable.java
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis;
+
+
+import javax.enterprise.inject.Stereotype;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Defines a class whose instances are stored as an intrinsic part of an owning entity and share the identity
+ * of the entity. Each of the persistent properties or fields of the embedded object is mapped to the database
+ * table for the entity.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Stereotype
+public @interface Embeddable {
+}
---
+++ artemis-configuration/src/main/java/org/jnosql/artemis/reflection/AbstractFieldRepresentation.java
@@ -0,0 +1,90 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.reflection;
+
+import org.jnosql.artemis.AttributeConverter;
+import org.jnosql.diana.api.Value;
+
+import java.lang.reflect.Field;
+import java.util.Optional;
+
+/**
+ * Base class to all {@link FieldRepresentation}
+ *
+ * @see FieldRepresentation
+ */
+abstract class AbstractFieldRepresentation implements FieldRepresentation {
+
+    protected final FieldType type;
+
+    protected final Field field;
+
+    protected final String name;
+
+    protected final String fieldName;
+
+    protected final Optional<Class<? extends AttributeConverter>> converter;
+
+    AbstractFieldRepresentation(FieldType type, Field field, String name, Class<? extends AttributeConverter> converter) {
+        this.type = type;
+        this.field = field;
+        this.name = name;
+        this.fieldName = field.getName();
+        this.converter = Optional.ofNullable(converter);
+    }
+
+    @Override
+    public FieldType getType() {
+        return type;
+    }
+
+    @Override
+    public Field getNativeField() {
+        return field;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getFieldName() {
+        return fieldName;
+    }
+
+
+    @Override
+    public <T extends AttributeConverter> Optional<Class<? extends AttributeConverter>> getConverter() {
+        return converter;
+    }
+
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("AbstractFieldRepresentation{");
+        sb.append("type=").append(type);
+        sb.append(", field=").append(field);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", fieldName='").append(fieldName).append('\'');
+        sb.append(", converter=").append(converter);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public Object getValue(Value value) {
+        return value.get(field.getType());
+    }
+}
