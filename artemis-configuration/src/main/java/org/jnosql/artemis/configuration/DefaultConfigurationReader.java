367bb9f42c9a9589234042978d07b6ca8e613c76
Otavio Santana <otaviopolianasantana@gmail.com>
2017-11-02 20:14:41 -0200

    removes the configuration reader

--- artemis-configuration/src/main/java/org/jnosql/artemis/configuration/DefaultConfigurationReader.java
+++ artemis-configuration/src/main/java/org/jnosql/artemis/configuration/DefaultConfigurationReader.java
@@ -14,6 +14,7 @@
  */
 package org.jnosql.artemis.configuration;
 
+import org.jnosql.artemis.ConfigurationReader;
 import org.jnosql.artemis.ConfigurationSettingsUnit;
 import org.jnosql.artemis.ConfigurationUnit;
 import org.jnosql.artemis.reflection.ConstructorException;
