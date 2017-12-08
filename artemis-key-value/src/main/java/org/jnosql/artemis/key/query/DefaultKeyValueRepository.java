45328ee2bfe168304996f6c0a352e85526bb8fcc
Otavio Santana <otaviopolianasantana@gmail.com>
2017-12-08 14:13:57 -0300

    implements abstract keyvalue

--- artemis-key-value/src/main/java/org/jnosql/artemis/key/query/DefaultKeyValueRepository.java
+++ artemis-key-value/src/main/java/org/jnosql/artemis/key/query/DefaultKeyValueRepository.java
@@ -1,54 +1,33 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
 package org.jnosql.artemis.key.query;
 
-import org.jnosql.artemis.Repository;
 import org.jnosql.artemis.key.KeyValueTemplate;
 
-import java.util.Optional;
+class DefaultKeyValueRepository<T>  extends AbstractKeyValueRepository<T> {
 
-class DefaultKeyValueRepository<T>  implements Repository {
-
-    private final Class<T> typeClass;
 
     private final KeyValueTemplate repository;
 
     public DefaultKeyValueRepository(Class<T> typeClass, KeyValueTemplate repository) {
-        this.typeClass = typeClass;
+        super(typeClass);
         this.repository = repository;
     }
 
-
     @Override
-    public Object save(Object entity) throws NullPointerException {
-        return repository.put(entity);
-    }
-
-    @Override
-    public Iterable save(Iterable entities) throws NullPointerException {
-        return repository.put(entities);
-    }
-
-    @Override
-    public void deleteById(Object key) throws NullPointerException {
-        repository.remove(key);
-    }
-
-    @Override
-    public void deleteById(Iterable ids) throws NullPointerException {
-        repository.remove(ids);
-    }
-
-    @Override
-    public Optional findById(Object key) throws NullPointerException {
-        return repository.get(key, typeClass);
-    }
-
-    @Override
-    public Iterable findById(Iterable keys) throws NullPointerException {
-        return repository.get(keys, typeClass);
-    }
-
-    @Override
-    public boolean existsById(Object key) throws NullPointerException {
-        return repository.get(key, typeClass).isPresent();
+    protected KeyValueTemplate getTemplate() {
+        return repository;
     }
 }
