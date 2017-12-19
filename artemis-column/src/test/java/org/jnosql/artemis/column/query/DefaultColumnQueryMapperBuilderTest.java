1100f6d7ee01fefddc8548c70189787e4294414b
Otavio Santana <otaviopolianasantana@gmail.com>
2017-12-19 05:31:09 -0300

    creates query mapper builder test

---
+++ artemis-column/src/test/java/org/jnosql/artemis/column/query/DefaultColumnQueryMapperBuilderTest.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.column.query;
+
+import org.jnosql.artemis.CDIJUnitRunner;
+import org.jnosql.artemis.column.ColumnQueryMapperBuilder;
+import org.jnosql.artemis.model.Person;
+import org.jnosql.diana.api.column.query.ColumnFrom;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(CDIJUnitRunner.class)
+public class DefaultColumnQueryMapperBuilderTest {
+
+
+    @Inject
+    private ColumnQueryMapperBuilder mapperBuilder;
+
+
+    @Test(expected = NullPointerException.class)
+    public void shouldReturnErrorWhenEntityClassIsNull() {
+        mapperBuilder.selectFrom(null);
+    }
+
+    @Test
+    public void shouldReturnSelectFrom() {
+        ColumnFrom columnFrom = mapperBuilder.selectFrom(Person.class);
+        assertNotNull(columnFrom);
+    }
+}
