cac7ba983695f47858f9976e458e17d10036eb92
Otavio Santana <otaviopolianasantana@gmail.com>
2017-12-19 05:14:45 -0300

    creates column query builder

---
+++ artemis-column/src/main/java/org/jnosql/artemis/column/DefaultColumnQueryMapperBuilder.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.column;
+
+import org.jnosql.artemis.reflection.ClassRepresentation;
+import org.jnosql.artemis.reflection.ClassRepresentations;
+import org.jnosql.diana.api.column.query.ColumnFrom;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+
+import static java.util.Objects.requireNonNull;
+
+@ApplicationScoped
+class DefaultColumnQueryMapperBuilder implements ColumnQueryMapperBuilder {
+
+    @Inject
+    private Instance<ClassRepresentations> classRepresentations;
+
+    @Override
+    public <T> ColumnFrom selectFrom(Class<T> entityClass) throws NullPointerException {
+        requireNonNull(entityClass, "entity is required");
+        ClassRepresentation representation = classRepresentations.get().get(entityClass);
+        return null;
+    }
+
+    @Override
+    public <T> ColumnFrom deleteFrom(Class<T> entityClass) throws NullPointerException {
+        requireNonNull(entityClass, "entity is required");
+        return null;
+    }
+}
