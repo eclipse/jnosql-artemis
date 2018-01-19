bfab3622d1a48fd7e3c80dfb5a6467cc2e9a0d0e
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-19 18:59:16 -0200

    fixes CDI extension

--- artemis-key-value/src/test/java/org/jnosql/artemis/CDIExtension.java
+++ artemis-key-value/src/test/java/org/jnosql/artemis/CDIExtension.java
@@ -20,18 +20,27 @@ import org.junit.jupiter.api.extension.TestInstancePostProcessor;
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
 import javax.inject.Inject;
+import javax.inject.Qualifier;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class CDIExtension implements TestInstancePostProcessor {
 
 
     private static final SeContainer CONTAINER = SeContainerInitializer.newInstance().initialize();
+    private static final Predicate<Annotation> FILTER = a -> a.annotationType().isAnnotationPresent(Qualifier.class);
+
     @Override
     public void postProcessTestInstance(Object testInstance, ExtensionContext context) throws IllegalAccessException {
 
         for (Field field : testInstance.getClass().getDeclaredFields()) {
             if(field.getAnnotation(Inject.class) != null) {
-                Object injected = CONTAINER.select(field.getType()).get();
+                Annotation[] qualifiers = Stream.of(field.getAnnotations())
+                        .filter(FILTER)
+                        .toArray(Annotation[]::new);
+                Object injected = CONTAINER.select(field.getType(), qualifiers).get();
                 field.setAccessible(true);
                 field.set(testInstance, injected);
             }
