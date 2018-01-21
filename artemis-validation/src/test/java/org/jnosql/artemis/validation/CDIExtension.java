24a9db767bafb45d552502409e31bfc1375f852d
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-20 23:57:42 -0200

    unitfy CDI contex

--- artemis-validation/src/test/java/org/jnosql/artemis/validation/CDIExtension.java
+++ artemis-validation/src/test/java/org/jnosql/artemis/validation/CDIExtension.java
@@ -23,33 +23,42 @@ import javax.inject.Inject;
 import javax.inject.Qualifier;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
 
 public class CDIExtension implements TestInstancePostProcessor {
 
 
     private static final SeContainer CONTAINER = SeContainerInitializer.newInstance().initialize();
-    private static final Predicate<Annotation> FILTER = a -> a.annotationType().isAnnotationPresent(Qualifier.class);
+    private static final Predicate<Annotation> IS_QUALIFIER = a -> a.annotationType().isAnnotationPresent(Qualifier.class);
 
     @Override
     public void postProcessTestInstance(Object testInstance, ExtensionContext context) throws IllegalAccessException {
 
-        for (Field field : testInstance.getClass().getDeclaredFields()) {
-            if(field.getAnnotation(Inject.class) != null) {
+        for (Field field : getFields(testInstance.getClass())) {
+            if (field.getAnnotation(Inject.class) != null) {
                 Annotation[] qualifiers = Stream.of(field.getAnnotations())
-                        .filter(FILTER)
+                        .filter(IS_QUALIFIER)
                         .toArray(Annotation[]::new);
                 Object injected = CONTAINER.select(field.getType(), qualifiers).get();
                 field.setAccessible(true);
                 field.set(testInstance, injected);
             }
-
         }
-
-
     }
 
-
+    private List<Field> getFields(Class<?> clazzInstance) {
+        List<Field> fields = new ArrayList<>();
+        if (!clazzInstance.getSuperclass().equals(Object.class)) {
+            fields.addAll(getFields(clazzInstance.getSuperclass()));
+        } else {
+            fields.addAll(asList(clazzInstance.getDeclaredFields()));
+        }
+        return fields;
+    }
 
 }
