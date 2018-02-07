5260006b1dc2bf4e4b5e5f538de71dfa23ee929f
Otavio Santana <otaviopolianasantana@gmail.com>
2018-02-07 20:24:20 -0200

    fixes NPE check

--- artemis-key-value/src/main/java/org/jnosql/artemis/key/AbstractKeyValueTemplate.java
+++ artemis-key-value/src/main/java/org/jnosql/artemis/key/AbstractKeyValueTemplate.java
@@ -26,6 +26,8 @@ import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * This class provides a skeletal implementation of the {@link KeyValueTemplate} interface,
  * to minimize the effort required to implement this interface.
@@ -41,6 +43,8 @@ public abstract class AbstractKeyValueTemplate implements KeyValueTemplate {
 
     @Override
     public <T> T put(T entity) {
+        requireNonNull(entity, "entity is required");
+
         UnaryOperator<KeyValueEntity<?>> putAction = k -> {
             getManager().put(k);
             return k;
@@ -51,6 +55,9 @@ public abstract class AbstractKeyValueTemplate implements KeyValueTemplate {
 
     @Override
     public <T> T put(T entity, Duration ttl) {
+        requireNonNull(entity, "entity is required");
+        requireNonNull(ttl, "ttl class is required");
+
         UnaryOperator<KeyValueEntity<?>> putAction = k -> {
             getManager().put(k, ttl);
             return k;
@@ -61,6 +68,9 @@ public abstract class AbstractKeyValueTemplate implements KeyValueTemplate {
 
     @Override
     public <K, T> Optional<T> get(K key, Class<T> clazz) {
+        requireNonNull(key, "key is required");
+        requireNonNull(clazz, "entity class is required");
+
         Optional<Value> value = getManager().get(key);
         return value.map(v -> getConverter().toEntity(clazz, v))
                 .filter(Objects::nonNull);
@@ -68,8 +78,8 @@ public abstract class AbstractKeyValueTemplate implements KeyValueTemplate {
 
     @Override
     public <K, T> Iterable<T> get(Iterable<K> keys, Class<T> clazz) {
-        Objects.requireNonNull(keys, "keys is required");
-        Objects.requireNonNull(clazz, "entity class is required");
+        requireNonNull(keys, "keys is required");
+        requireNonNull(clazz, "entity class is required");
         return StreamSupport.stream(getManager()
                 .get(keys).spliterator(), false)
                 .map(v -> getConverter().toEntity(clazz, v))
@@ -79,11 +89,13 @@ public abstract class AbstractKeyValueTemplate implements KeyValueTemplate {
 
     @Override
     public <K> void remove(K key) {
+        requireNonNull(key, "key is required");
         getManager().remove(key);
     }
 
     @Override
     public <K> void remove(Iterable<K> keys) {
+        requireNonNull(keys, "keys is required");
         getManager().remove(keys);
     }
 }
