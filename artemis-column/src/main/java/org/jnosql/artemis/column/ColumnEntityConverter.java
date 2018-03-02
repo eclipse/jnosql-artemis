6eaa61d4ebccedec7e6736d16717e091434fa2b0
Otavio Santana <otaviopolianasantana@gmail.com>
2018-03-02 07:30:50 -0300

    fixes javadoc

--- artemis-column/src/main/java/org/jnosql/artemis/column/ColumnEntityConverter.java
+++ artemis-column/src/main/java/org/jnosql/artemis/column/ColumnEntityConverter.java
@@ -46,13 +46,13 @@ public interface ColumnEntityConverter {
      * Converts a {@link ColumnEntity} to entity
      * Instead of creating a new object is uses the instance used in this parameters
      *
-     * @param instance the instance
+     * @param entityInstance the instance
      * @param entity      the {@link ColumnEntity} to be converted
      * @param <T>         the entity type
      * @return the same instance with values set from {@link ColumnEntity}
-     * @throws NullPointerException when either instance or entity are null
+     * @throws NullPointerException when either entityInstance or entity are null
      */
-    <T> T toEntity(T instance, ColumnEntity entity);
+    <T> T toEntity(T entityInstance, ColumnEntity entity);
 
     /**
      * Similar to {@link ColumnEntityConverter#toEntity(Class, ColumnEntity)}, but
