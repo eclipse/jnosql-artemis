5f4630553665bb9a0d5c8ec1319fb23a013097d6
Otavio Santana <otaviopolianasantana@gmail.com>
2018-05-24 18:56:32 -0300

    fixes the metadata

--- artemis-core/src/main/java/org/jnosql/artemis/DatabaseMetadata.java
+++ artemis-core/src/main/java/org/jnosql/artemis/DatabaseMetadata.java
@@ -21,10 +21,10 @@ import java.util.Objects;
  */
 public final class DatabaseMetadata {
 
-    public static final DatabaseMetadata DEFAULT_KEY_VALUE = new DatabaseMetadata(DatabaseType.COLUMN, "");
+    public static final DatabaseMetadata DEFAULT_KEY_VALUE = new DatabaseMetadata(DatabaseType.KEY_VALUE, "");
     public static final DatabaseMetadata DEFAULT_COLUMN = new DatabaseMetadata(DatabaseType.COLUMN, "");
-    public static final DatabaseMetadata DEFAULT_DOCUMENT = new DatabaseMetadata(DatabaseType.COLUMN, "");
-    public static final DatabaseMetadata DEFAULT_GRAPH = new DatabaseMetadata(DatabaseType.COLUMN, "");
+    public static final DatabaseMetadata DEFAULT_DOCUMENT = new DatabaseMetadata(DatabaseType.DOCUMENT, "");
+    public static final DatabaseMetadata DEFAULT_GRAPH = new DatabaseMetadata(DatabaseType.GRAPH, "");
     private final DatabaseType type;
 
     private final String provider;
