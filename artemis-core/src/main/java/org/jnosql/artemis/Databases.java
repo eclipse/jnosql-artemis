0f67f7adebe8313941f14ee8ccd09ac4cdcf728d
Otavio Santana <otaviopolianasantana@gmail.com>
2018-05-24 18:36:10 -0300

    moves to use dastabasemedata to avoid duplicated instances

--- artemis-core/src/main/java/org/jnosql/artemis/Databases.java
+++ artemis-core/src/main/java/org/jnosql/artemis/Databases.java
@@ -16,7 +16,6 @@ package org.jnosql.artemis;
 
 import javax.enterprise.inject.spi.ProcessProducer;
 import java.lang.annotation.Annotation;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.logging.Logger;
@@ -28,20 +27,19 @@ public final class Databases {
 
     private static final Logger LOGGER = Logger.getLogger(Databases.class.getName());
 
-    private Databases() {}
+    private Databases() {
+    }
 
     /**
      * This method get the class from ProcessProduce, check if the object is a valid object for the database type
      * add it for a list passed for CDI's lifecycle.
      *
      * @param processProducer the {@link ProcessProducer} of CDI Exntesion
-     * @param type type data which extension is scanning
-     * @param databases list of objects which will be used by Artemis CDI Extension
-     *
+     * @param type            type data which extension is scanning
+     * @param databases       list of objects which will be used by Artemis CDI Extension
      * @see DatabaseType
-     *
      */
-    public static void addDatabase(final ProcessProducer processProducer, final DatabaseType type, final List<Database> databases) {
+    public static void addDatabase(final ProcessProducer processProducer, final DatabaseType type, final Set<DatabaseMetadata> databases) {
         LOGGER.info(String.format("Found the type %s to databases %s", type, databases));
         Set<Annotation> annotations = processProducer.getAnnotatedMember().getAnnotations();
         Optional<Database> databaseOptional = annotations.stream().filter(a -> a instanceof Database)
@@ -51,7 +49,7 @@ public final class Databases {
                 String simpleName = processProducer.getAnnotatedMember().getDeclaringType().getJavaClass().getSimpleName();
                 throw new IllegalStateException(String.format("The %s is producing a wrong manager for %s type", simpleName, type));
             }
-            databases.add(database);
+            databases.add(DatabaseMetadata.of(database));
         });
     }
 }
