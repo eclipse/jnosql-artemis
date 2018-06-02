bf581f9f65837dfe09c60c88b429119f4d7ab300
Otavio Santana <otaviopolianasantana@gmail.com>
2018-06-02 12:36:16 -0300

    parser

--- artemis-column/src/main/java/org/jnosql/artemis/column/MapperColumnQueryParser.java
+++ artemis-column/src/main/java/org/jnosql/artemis/column/MapperColumnQueryParser.java
@@ -2,32 +2,17 @@ package org.jnosql.artemis.column;
 
 import org.jnosql.artemis.reflection.ClassRepresentation;
 import org.jnosql.artemis.reflection.ClassRepresentations;
-import org.jnosql.diana.api.column.ColumnEntity;
-import org.jnosql.diana.api.column.ColumnFamilyManager;
-import org.jnosql.diana.api.column.ColumnPreparedStatement;
-import org.jnosql.diana.api.column.ColumnQueryParser;
+import org.jnosql.diana.api.column.query.DefaultColumnQueryParser;
 
-import java.util.List;
 import java.util.Optional;
 
-final class MapperColumnQueryParser implements ColumnQueryParser {
+final class MapperColumnQueryParser extends DefaultColumnQueryParser {
 
-    private final ColumnQueryParser parser = ColumnQueryParser.getParser();
 
     private final ClassRepresentations representations;
 
     MapperColumnQueryParser(ClassRepresentations representations) {
         this.representations = representations;
-    }
-
-    @Override
-    public List<ColumnEntity> query(String query, ColumnFamilyManager manager) {
-        return parser.query(query, manager);
-    }
-
-    @Override
-    public ColumnPreparedStatement prepare(String query, ColumnFamilyManager manager) {
-        return parser.prepare(query, manager);
     }
 
     public String fireEntity(String entity) {
