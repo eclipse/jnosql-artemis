94a809277e81b848ba82985cfb93eb9cd05195b4
Otavio Santana <otaviopolianasantana@gmail.com>
2018-06-02 12:30:14 -0300

    implementes parsre

--- artemis-column/src/main/java/org/jnosql/artemis/column/MapperColumnQueryParser.java
+++ artemis-column/src/main/java/org/jnosql/artemis/column/MapperColumnQueryParser.java
@@ -1,6 +1,7 @@
 package org.jnosql.artemis.column;
 
 import org.jnosql.artemis.reflection.ClassInformationNotFoundException;
+import org.jnosql.artemis.reflection.ClassRepresentation;
 import org.jnosql.artemis.reflection.ClassRepresentations;
 import org.jnosql.diana.api.column.ColumnEntity;
 import org.jnosql.diana.api.column.ColumnFamilyManager;
@@ -8,6 +9,7 @@ import org.jnosql.diana.api.column.ColumnPreparedStatement;
 import org.jnosql.diana.api.column.ColumnQueryParser;
 
 import java.util.List;
+import java.util.Optional;
 
 final class MapperColumnQueryParser implements ColumnQueryParser {
 
@@ -30,15 +32,24 @@ final class MapperColumnQueryParser implements ColumnQueryParser {
     }
 
     public String fireEntity(String entity) {
-        try {
-            representations.findBySimpleClassName(entity);
-        } catch (ClassInformationNotFoundException e) {
-            return entity;
-        }
+        Optional<ClassRepresentation> classRepresentation = getClassRepresentation(entity);
+        return classRepresentation.map(ClassRepresentation::getName).orElse(entity);
     }
 
     public String fireField(String entity, String field) {
-        return field;
+        Optional<ClassRepresentation> classRepresentation = getClassRepresentation(entity);
+        return classRepresentation.map(c -> c.getColumnField(field)).orElse(field);
     }
 
+    private Optional<ClassRepresentation> getClassRepresentation(String entity) {
+        Optional<ClassRepresentation> bySimpleName = representations.findBySimpleName(entity);
+        if (bySimpleName.isPresent()) {
+            return bySimpleName;
+        }
+        Optional<ClassRepresentation> byClassName = representations.findByClassName(entity);
+        if (byClassName.isPresent()) {
+            return byClassName;
+        }
+        return Optional.empty();
+    }
 }
