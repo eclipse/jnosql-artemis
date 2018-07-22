19923241c5e235b1f651ddf186f7cdd53ee2fc0f
Otavio Santana <otaviopolianasantana@gmail.com>
2018-07-22 11:39:43 -0300

    fixes template documentation

--- artemis-document/src/main/java/org/jnosql/artemis/document/query/DocumentMapperDeleteQueryBuild.java
+++ artemis-document/src/main/java/org/jnosql/artemis/document/query/DocumentMapperDeleteQueryBuild.java
@@ -6,7 +6,7 @@ import org.jnosql.diana.api.document.DocumentDeleteQuery;
 /**
  * The last step to the build of {@link DocumentDeleteQuery}.
  * It either can return a new {@link DocumentDeleteQuery} instance or execute a query with
- * {@link org.jnosql.diana.api.document.DocumentCollectionManager} and {@link DocumentCollectionManagerAsync}
+ * {@link org.jnosql.artemis.document.DocumentTemplate} and {@link org.jnosql.artemis.document.DocumentTemplateAsync}
  */
 public interface DocumentMapperDeleteQueryBuild {
     /**
