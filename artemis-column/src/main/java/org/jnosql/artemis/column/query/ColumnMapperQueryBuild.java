6311c5bd8afe55655ba9250100dd8c6e92ff97dc
Otavio Santana <otaviopolianasantana@gmail.com>
2018-07-22 12:57:48 -0300

    fixes column implementation

---
+++ artemis-column/src/main/java/org/jnosql/artemis/column/query/ColumnMapperQueryBuild.java
@@ -0,0 +1,35 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+package org.jnosql.artemis.column.query;
+
+import org.jnosql.diana.api.column.ColumnQuery;
+
+/**
+ * The last step to the build of {@link ColumnQuery}.
+ * It either can return a new {@link ColumnQuery} instance or execute a query with
+ * {@link org.jnosql.artemis.column.ColumnTemplate} and {@link org.jnosql.artemis.column.ColumnTemplateAsync}
+ */
+public interface ColumnMapperQueryBuild {
+
+    /**
+     * Creates a new instance of {@link ColumnQuery}
+     *
+     * @return a new {@link ColumnQuery} instance
+     */
+    ColumnQuery build();
+
+}
