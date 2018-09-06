b38b4082587b098253c0430b7cf8e2fb610e3742
Otavio Santana <otaviopolianasantana@gmail.com>
2018-09-06 17:26:02 -0300

    fixes coverter util

---
+++ artemis-document/src/main/java/org/jnosql/artemis/document/query/ParamsBinder.java
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.document.query;
+
+
+import org.jnosql.artemis.Converters;
+import org.jnosql.artemis.DynamicQueryException;
+import org.jnosql.artemis.document.util.ConverterUtil;
+import org.jnosql.artemis.reflection.ClassRepresentation;
+import org.jnosql.artemis.reflection.FieldRepresentation;
+import org.jnosql.query.Params;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Optional;
+
+class ParamsBinder {
+
+    private final ClassRepresentation representation;
+
+    private final Converters converters;
+
+
+    ParamsBinder(ClassRepresentation representation, Converters converters) {
+        this.representation = representation;
+        this.converters = converters;
+    }
+
+    public void bind(Params params, Object[] args, Method method) {
+
+        List<String> names = params.getNames();
+        if (names.size() > args.length) {
+            throw new DynamicQueryException("The number of parameters in a query is bigger than the number of " +
+                    "parameters in the method: " + method);
+        }
+        for (int index = 0; index < names.size(); index++) {
+            String name = names.get(index);
+            String fieldName = name.substring(0, name.lastIndexOf("_"));
+            Optional<FieldRepresentation> field = this.representation.getFields().stream()
+                    .filter(f -> f.getName().equals(fieldName)).findFirst();
+
+            Object value;
+            if (field.isPresent()) {
+                value = ConverterUtil.getValue(args[index], converters, field.get());
+            } else {
+                value = args[index];
+            }
+            params.bind(name, value);
+        }
+    }
+}
