d2cbe4fd3ba4f056b6cdc5bc32eea4f9ba285c2e
Otavio Santana <otaviopolianasantana@gmail.com>
2018-09-08 07:00:14 -0300

    fixes tests

--- artemis-column/src/test/java/org/jnosql/artemis/column/query/ColumnRepositoryProxyTest.java
+++ artemis-column/src/test/java/org/jnosql/artemis/column/query/ColumnRepositoryProxyTest.java
@@ -380,11 +380,11 @@ public class ColumnRepositoryProxyTest {
 
         assertEquals(Condition.EQUALS, columnCondition.getCondition());
         assertEquals("Ada", columnCondition.getColumn().get());
-        assertTrue(columnCondition.getColumn().getName().contains("name"));
+        assertTrue(columnCondition.getColumn().getName().equals("name"));
 
         assertEquals(Condition.GREATER_EQUALS_THAN, columnCondition2.getCondition());
         assertEquals(33, columnCondition2.getColumn().get());
-        assertTrue(columnCondition2.getColumn().getName().contains("age"));
+        assertTrue(columnCondition2.getColumn().getName().equals("age"));
     }
 
     @Test
