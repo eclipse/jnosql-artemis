9a379fad0943cc5b3d29dfa284732fc46247af4f
Otavio Santana <otaviopolianasantana@gmail.com>
2018-09-17 21:15:22 -0300

    creates query

---
+++ artemis-graph/src/main/java/org/jnosql/artemis/graph/query/GraphQueryMethod.java
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.graph.query;
+
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.jnosql.artemis.Converters;
+import org.jnosql.artemis.DynamicQueryException;
+import org.jnosql.artemis.reflection.ClassRepresentation;
+import org.jnosql.artemis.util.ConverterUtil;
+import org.jnosql.query.SelectQuery;
+
+import java.lang.reflect.Method;
+
+final class GraphQueryMethod {
+
+    private final SelectQuery query;
+    private final ClassRepresentation representation;
+    private final GraphTraversal<Vertex, Vertex> traversal;
+    private final Object[] args;
+    private final Converters converters;
+    private final Method method;
+    private int counter = 0;
+
+    GraphQueryMethod(SelectQuery query, ClassRepresentation representation,
+                     GraphTraversal<Vertex, Vertex> traversal,
+                     Object[] args, Converters converters, Method method) {
+        this.query = query;
+        this.representation = representation;
+        this.traversal = traversal;
+        this.args = args;
+        this.converters = converters;
+        this.method = method;
+    }
+
+    public SelectQuery getQuery() {
+        return query;
+    }
+
+    public ClassRepresentation getRepresentation() {
+        return representation;
+    }
+
+    public GraphTraversal<Vertex, Vertex> getTraversal() {
+        return traversal;
+    }
+
+    public Object getValue(String name) {
+        if ((counter + 1) > args.length) {
+            throw new DynamicQueryException(String.format("There is a missed argument in the method %s",
+                    method));
+        }
+        Object value = args[counter];
+        counter++;
+        return ConverterUtil.getValue(value, representation, name, converters);
+    }
+}
---
+++ artemis-graph/src/main/java/org/jnosql/artemis/graph/query/SelectQueryConverter.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.graph.query;
+
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.jnosql.query.Condition;
+import org.jnosql.query.Operator;
+import org.jnosql.query.SelectQuery;
+import org.jnosql.query.Where;
+
+import java.util.List;
+import java.util.function.Function;
+
+class SelectQueryConverter implements Function<GraphQueryMethod, List<Vertex>> {
+
+
+    @Override
+    public List<Vertex> apply(GraphQueryMethod graphQuery) {
+
+        SelectQuery query = graphQuery.getQuery();
+        GraphTraversal<Vertex, Vertex> traversal = graphQuery.getTraversal();
+        if (query.getWhere().isPresent()) {
+            Where where = query.getWhere().get();
+
+            Condition condition = where.getCondition();
+            Operator operator = condition.getOperator();
+            String name = condition.getName();
+            switch (operator) {
+                case EQUALS:
+                    traversal.has(name, graphQuery.getValue())
+                case IN:
+                case GREATER_THAN:
+                case GREATER_EQUALS_THAN:
+                case LESSER_THAN:
+                case LESSER_EQUALS_THAN:
+                case LIKE:
+                case AND:
+                case OR:
+                case NOT:
+                case BETWEEN:
+                default:
+                    throw new UnsupportedOperationException("There is not support to the type " + operator + " in graph");
+            }
+        }
+
+        return null;
+    }
+}
