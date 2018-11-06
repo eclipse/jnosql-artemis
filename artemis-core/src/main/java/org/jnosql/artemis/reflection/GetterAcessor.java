698291f8ab2a21a5d4d04a83f0bbda95419dd097
Otavio Santana <otaviopolianasantana@gmail.com>
2018-11-06 17:31:14 -0200

    adds getter acessor

---
+++ artemis-core/src/main/java/org/jnosql/artemis/reflection/GetterAcessor.java
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.reflection;
+
+
+/**
+ * This class represents given a Field, the value within a Bean instance.
+ */
+public interface GetterAcessor {
+
+    /**
+     * From the entity bean, it will read the respective field and return the value.
+     *
+     * @param bean the entity that has the field
+     * @return the field value from the entity
+     */
+    Object get(Object bean);
+}
