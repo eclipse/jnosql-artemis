f7edcff8afd1070436f2eb560ed6d27b3aae7015
Otavio Santana <otaviopolianasantana@gmail.com>
2018-11-22 16:59:35 -0200

    Fixes the GraphProducer implementation

--- artemis-graph/src/main/java/org/jnosql/artemis/graph/GraphProducer.java
+++ artemis-graph/src/main/java/org/jnosql/artemis/graph/GraphProducer.java
@@ -14,5 +14,47 @@
  */
 package org.jnosql.artemis.graph;
 
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.jnosql.artemis.ConfigurationReader;
+import org.jnosql.artemis.ConfigurationSettingsUnit;
+import org.jnosql.artemis.ConfigurationUnit;
+import org.jnosql.artemis.reflection.Reflections;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.Annotated;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+
+import static org.jnosql.artemis.util.ConfigurationUnitUtils.getConfigurationUnit;
+
+@ApplicationScoped
 class GraphProducer {
+
+    @Inject
+    private Reflections reflections;
+
+    @Inject
+    private Instance<ConfigurationReader> configurationReader;
+
+    @ConfigurationUnit
+    @Produces
+    public Graph getBucketManagerGenerics(InjectionPoint injectionPoint) {
+        return getBuckerManagerFactocy(injectionPoint);
+    }
+
+    private Graph getBuckerManagerFactocy(InjectionPoint injectionPoint) {
+        Annotated annotated = injectionPoint.getAnnotated();
+        ConfigurationUnit annotation = getConfigurationUnit(injectionPoint, annotated)
+                .orElseThrow(() -> new IllegalStateException("The @ConfigurationUnit does not found"));
+
+        ConfigurationSettingsUnit unit = configurationReader.get().read(annotation, GraphFactory.class);
+        Class<GraphFactory> configurationClass = unit.<GraphFactory>getProvider()
+                .orElseThrow(() -> new IllegalStateException("The ColumnConfiguration provider is required in the configuration"));
+
+        GraphFactory factory = reflections.newInstance(configurationClass);
+
+        return factory.apply(unit.getSettings());
+    }
 }
