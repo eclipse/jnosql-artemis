1ce998304acc4cfa281c95f4cf6d3144b46a31f7
Otavio Santana <otaviopolianasantana@gmail.com>
2019-01-29 17:29:40 -0200

    creates keyvalue template to configuration factory

---
+++ artemis-key-value/src/main/java/org/jnosql/artemis/key/KeyValueTemplateConfigurationFactory.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.key;
+
+import org.jnosql.artemis.ConfigurationUnit;
+import org.jnosql.diana.api.key.BucketManager;
+import org.jnosql.diana.api.key.BucketManagerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+
+/**
+ *
+ */
+@ApplicationScoped
+class KeyValueTemplateConfigurationFactory {
+
+    @Inject
+    private BucketManagerFactoryProducer bucketProducer;
+
+    @Inject
+    private KeyValueTemplateProducer keyValueTemplateProducer;
+
+    @ConfigurationUnit
+    @Produces
+    public <T extends KeyValueTemplate> T getKeyValueTemplate(InjectionPoint injectionPoint) {
+        return getBuckerManagerFactocy(injectionPoint);
+    }
+}
