182777f691d125ad3ec7afc418ffc5df21e55509
Otavio Santana <otaviopolianasantana@gmail.com>
2019-01-30 18:58:47 -0200

    moves the configuration factory to spi

--- artemis-document/src/main/java/org/jnosql/artemis/document/spi/DocumentCollectionFactoryProducer.java
+++ artemis-document/src/main/java/org/jnosql/artemis/document/spi/DocumentCollectionFactoryProducer.java
@@ -12,7 +12,7 @@
  *
  *   Otavio Santana
  */
-package org.jnosql.artemis.document;
+package org.jnosql.artemis.document.spi;
 
 import org.jnosql.artemis.ConfigurationReader;
 import org.jnosql.artemis.ConfigurationSettingsUnit;
