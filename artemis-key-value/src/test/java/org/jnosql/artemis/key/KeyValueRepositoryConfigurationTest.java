643e9f6aba179a53c04f1833a55c06379aaf877c
Otavio Santana <otaviopolianasantana@gmail.com>
2019-01-30 20:03:13 -0200

    fixes test name to repository configuration

--- artemis-key-value/src/test/java/org/jnosql/artemis/key/KeyValueRepositoryConfigurationTest.java
+++ artemis-key-value/src/test/java/org/jnosql/artemis/key/KeyValueRepositoryConfigurationTest.java
@@ -24,7 +24,7 @@ import javax.inject.Inject;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(CDIExtension.class)
-class KeyValueConfigurationUnitExtensionTest {
+class KeyValueRepositoryConfigurationTest {
 
 
     @Inject
